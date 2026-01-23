da90147b0143f4c70b8d5d56420b55812e1585ef
Hondask <ankostiuk96@gmail.com>
2026-01-24 00:16:27 +0300

    Подключение логирования Serilog

--- USG_Tools.CLI/Program.cs
+++ USG_Tools.CLI/Program.cs
@@ -1,6 +1,7 @@
-﻿
-
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using USG_Tools.Core.Managers;
 
 namespace USG_Tools.CLI
@@ -10,18 +11,42 @@ namespace USG_Tools.CLI
         static async Task Main(string[] args)
         {
 
-            // Создаем фабрику логов 
-            using var loggerFactory = LoggerFactory.Create(builder => {
-                builder.AddConsole();
-            });
-            ILogger logger = loggerFactory.CreateLogger<Program>();
+            // Берем конфигурацию логера из json 
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("LogSettings.json", optional: false, reloadOnChange: true)
+                .Build();
 
-            //Инициализируем конфиг
-            var configManager = new ConfigManager(loggerFactory.CreateLogger<ConfigManager>());
+            // Настройка Serilog
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            try
+            {
+                // 3. Настройка DI-контейнера (если используешь) или LoggerFactory
+                using var loggerFactory = LoggerFactory.Create(builder =>
+                {
+                    builder.AddSerilog(); // Используем Serilog под капотом Microsoft Logging
+                });
 
-            // Запуск меню
-            var menu = new Menu(configManager, loggerFactory.CreateLogger<Menu>());
-            await menu.RunAsync();
+                // 4. Создание зависимостей
+                var configLogger = loggerFactory.CreateLogger<ConfigManager>();
+                var configManager = new ConfigManager(configLogger);
+
+                var menuLogger = loggerFactory.CreateLogger<Menu>();
+                var menu = new Menu(configManager, menuLogger);
+
+                // 5. Запуск
+                await menu.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Приложение аварийно завершилось");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
